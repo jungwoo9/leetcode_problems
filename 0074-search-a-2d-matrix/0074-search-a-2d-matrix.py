75b260e4c062fac417a1bfd88ac681954429abdd
Jungwoo Koo <95963429+jungwoo9@users.noreply.github.com>
2023-08-20 16:48:09 +0900

    Time: 41 ms (96.82%), Space: 16.8 MB (58.50%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -4,17 +4,13 @@ class Solution:
         
         while l <= r:
             m = (l+r)//2
-            print(l, r, m)
-            # if l == m:
-            #     break
             if matrix[m][-1] < target:
                 l += 1
             elif matrix[m][0] > target:
                 r -= 1
             else:
                 break
-        # print(l, r, m)
-        # m=l
+        
         ll, rr = 0, len(matrix[m])-1
         
         while ll <= rr:
